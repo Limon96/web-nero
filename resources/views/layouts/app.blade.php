2688a00879e6c19ea28d944d8673e025f910fc1e
Tatmosiy123 <tatmosiy123@gmail.com>
2022-10-30 06:34:34 +0300

    sitemap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://studentik.online/image/catalog/favicon-32x32.png" rel="icon" />
     <title>{{ config('app.name', 'Laravel') }}</title>
-    метрика2
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
