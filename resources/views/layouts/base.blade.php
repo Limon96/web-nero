8b2fd265c52a764281dae4f8f653c30ee824a3c9
Tatmosiy123 <tatmosiy123@gmail.com>
2022-10-30 06:27:02 +0300

    гугл аналитика

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -31,7 +31,15 @@
 
     @yield('styles')
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-99H8SP7VYC"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'G-99H8SP7VYC');
+    </script>
 <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
