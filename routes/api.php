bbbaa348af60f197f9643c7e9db1db6ae71e3279
Limon96 <nicker08@inbox.ru>
2023-05-01 17:01:27 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('smtp', [\App\Http\Controllers\API\SMTPController::class, 'send'])->middleware('cors');
+Route::options('smtp', function () {
+    return 'Hello World';
+})->middleware('cors');
