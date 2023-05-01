92aff12d539ecae46df602b728faf7db531c569a
Limon96 <nicker08@inbox.ru>
2023-05-01 14:26:06 +0300

    Отправка писем с insales

---
+++ app/Http/Controllers/API/SMTPController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SMTPRequest;
+use App\Mail\SMTP;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Mail;
+
+class SMTPController extends Controller
+{
+
+    /**
+     * @param SMTPRequest $request
+     * @return JsonResponse
+     */
+    public function send(SMTPRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $data = $request->validated();
+
+        $message = $this->message($data);
+
+        Mail::to('nicker08@inbox.ru')->send(new SMTP(
+            $data['subject'],
+            $message,
+        ));
+
+        return response()
+            ->json([
+                'success' => 1
+            ]);
+    }
+
+    private function message($data)
+    {
+        return "
+            <h1>{$data['subject']}</h1>\n
+            <p><b>Email:</b>  {$data['email']}</p>\n
+            <p><b>Вопрос:</b>  {$data['content']}</p>
+        ";
+    }
+}
